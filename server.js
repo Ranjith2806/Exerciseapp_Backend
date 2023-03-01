09399be5c5758c503e66270356dacccc108ae3b6
Ranjith2806 <ranjithkumars2806@gmail.com>
2023-03-01 21:59:43 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   .env
    	new file:   .gitignore
    	new file:   models/excerise.model.js
    	new file:   models/user.model.js
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   routes/exercises.js
    	new file:   routes/users.js
    	new file:   server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+// const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require ('express');
+const mongoose = require('mongoose');
+
+
+
+require('dotenv').config();
+
+const app = express();
+
+const port = 80;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.MONGO_URI
+
+mongoose.connect(uri, {useNewUrlParser:true , useCreateIndex: true})
+
+const connection = mongoose.connection;
+
+
+connection.once('open',() => {
+    console.log("MongoDB database connection established");
+})
+
+
+const userRoutes = require('./routes/users');
+const exceriseRoutes = require('./routes/exercises');
+
+
+app.use('/users',userRoutes);
+app.use('/exercises', exceriseRoutes);
+
+
+
+
+app.listen(port,() => {
+  
+    console.log(`Server is running on the: ${port}`);
+})
